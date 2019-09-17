76a5b852773fbf697f5afe6a96646f18456cc847
Micu Alexandru-Vlad <micualexandru131@yahoo.com>
2019-09-18 00:44:51 +0300

    commit

---
+++ app/src/main/java/com/example/pricer/SplashActivity.kt
@@ -0,0 +1,46 @@
+package com.example.pricer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.widget.ImageView
+import android.widget.ProgressBar
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var loading: ProgressBar
+    private lateinit var logo: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        loading = findViewById(R.id.pb_loading)
+        logo = findViewById(R.id.iv_logo)
+
+        val fadeInLoading = AlphaAnimation(0f, 1f)
+        fadeInLoading.startOffset = 1000
+        fadeInLoading.duration = 200
+        loading.startAnimation(fadeInLoading)
+
+        val fadeInLogo = AlphaAnimation(0f, 1f)
+        fadeInLogo.duration = 500
+        logo.startAnimation(fadeInLogo)
+
+        fadeInLoading.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(p0: Animation?) {
+
+            }
+
+            override fun onAnimationStart(p0: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(p0: Animation?) {
+
+            }
+
+        })
+    }
+}
